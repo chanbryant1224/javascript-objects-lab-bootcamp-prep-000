3047bc826c5593e4fc8da1d53e69e6ee0f035f14
Wing Chak Chan <chanbryant1224@gmail.com>
2017-11-18 22:30:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var recipes = {}
 function updateObjectWithKeyAndValue (object, key, value){return Object.assign({},object,{[key]:value})}
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value){ object[key] = value
   return object}
-function destructivelydeleteFromObjectByKey (object, key){delete object[key]
+function destructivelyDeleteFromObjectByKey (object, key){delete object[key]
 return object}
